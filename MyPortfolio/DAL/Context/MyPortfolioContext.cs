d990ef354f851dfeedd0459ecce8235300b935f1
Boran Kaya <borankaya210@gmail.com>
2024-07-07 03:03:02 +0300

    Eklemeler Yapıldı.

---
+++ MyPortfolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.DAL.Context
+{
+    public class MyPortfolioContext :  DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;initial Catalog=MyPortfolioDb; integrated Security =true;");
+        }
+        public DbSet<About> abouts { get; set; }
+        public DbSet<Contact> contacts { get; set; }
+        public DbSet<Experience> experiences { get; set; }
+        public DbSet<Fature> fatures { get; set; }
+        public DbSet<Message> messages { get; set; }
+        public DbSet<Portfolio> portfolios { get; set; }
+        public DbSet<Skill> skills { get; set; }
+        public DbSet<SocialMedia> socialMedias { get; set; }
+        public DbSet<Testimonial> testimonials { get; set; }
+    }
+}
---
+++ MyPortfolio/DAL/Entities/Fature.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Fature
+    {
+        public int FatureId { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
